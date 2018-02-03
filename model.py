4b2669a82167abc19a92c7a6ca49df0dab47b3dc
Nafis Jamal <nafisjamal@gmail.com>
2018-02-03 09:35:20 -0800

    fixed typo

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ import numpy as np
 lines = []
 with open('../data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
+	reader.next() # skip header
 	for line in reader:
 		lines.append(line)
 
