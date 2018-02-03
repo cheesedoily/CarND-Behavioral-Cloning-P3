6222b879cbe849c5c755173c8000ef8b39af0cc0
Nafis Jamal <nafisjamal@gmail.com>
2018-02-03 09:38:38 -0800

    fixed typo

--- model.py
+++ model.py
@@ -28,10 +28,10 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-models.add(Flatten(input_shape(160,320,3)))
+model.add(Flatten(input_shape(160,320,3)))
 model.add(Dense(1))
 
-models.compile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
