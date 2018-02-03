3d4bba5ae3a51e18b793125b2fb55b78d20a58de
Nafis Jamal <nafisjamal@gmail.com>
2018-02-03 10:53:16 -0800

    fixed typo

--- model.py
+++ model.py
@@ -19,20 +19,20 @@ for line in lines:
     steering_right = steering_center - correction
 
     path = '../data/'
-    img_center = cv2.imread(path + line[0])
-    img_left = cv2.imread(path + line[1])
-    img_right = cv2.imread(path + line[2])
+    img_center = cv2.imread(path + line[0].strip())
+    img_left = cv2.imread(path + line[1].strip())
+    img_right = cv2.imread(path + line[2].strip())
 
     # print("{}--{}".format(steering_center, path + line[0]))
     # print("{}--{}".format(steering_left, path + line[1]))
     # print("{}--{}".format(steering_right, path + line[2]))
 
     images.append(img_center) 
-    # images.append(img_left)
+    images.append(img_left)
     images.append(img_right)
 
     measurements.append(steering_center)
-    # measurements.append(steering_left)
+    measurements.append(steering_left)
     measurements.append(steering_right)
 
     # measurements += [steering_center, steering_left, steering_right]
