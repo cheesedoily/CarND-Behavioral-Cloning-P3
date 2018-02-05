869ce3256a09282b3ae3c8431537358c3961b599
Nafis Jamal <nafisjamal@gmail.com>
2018-02-05 14:30:57 -0800

    fixed typo

--- clone.py
+++ clone.py
@@ -61,6 +61,7 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
+"""
 images = []
 measurements = []
 for line in lines:
@@ -103,7 +104,7 @@ for image, measurement in zip(images, measurements):
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
-
+"""
 
 
 from keras.models import Sequential
@@ -112,17 +113,21 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers.convolutional import Cropping2D
 
-model = Sequential()
-model.add(Cropping2D(cropping=((70, 25), (0,0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(65, 320, 3)))
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6, 5, 5, activation='relu'))
-model.add(MaxPooling2D())
-model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
+def make_model():
+    model = Sequential()
+    model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((70, 25), (0,0)),))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
 
 model.compile(loss='mse', optimizer='adam')
 # model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
