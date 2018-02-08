0d4f28975a77de92604cf6e5b592a73524cfc369
Nafis Jamal <nafisjamal@gmail.com>
2018-02-07 17:32:30 -0800

    fixed typo

--- clone.py
+++ clone.py
@@ -77,7 +77,7 @@ from keras.layers.convolutional import Cropping2D
 def make_model():
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
-    model.add(Cropping2D(cropping=((50, 25), (0,0)),))
+    model.add(Cropping2D(cropping=((70, 25), (0,0)),))
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
